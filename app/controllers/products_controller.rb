8517496477454e5f1a467482617e967a80cf037b
Daniel Dizzy <dizzylol4@gmail.com>
2016-02-15 15:58:06 +0900

    Added product user association

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  #ENsure the user is logged in
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
 
   # GET /products
   # GET /products.json
@@ -24,7 +27,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @product = current_user.products.new(product_params)
 
     respond_to do |format|
       if @product.save
